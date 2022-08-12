a4972f0cddf40e5a2a32018fdc74249ce71522a6
tkam627 <52318237+Takashi-Kamata@users.noreply.github.com>
2022-08-12 16:40:09 +1200

    working

--- main.c
+++ main.c
@@ -22,9 +22,16 @@ asm(".global _printf_float");
 #define CLEAR_STRING "\033[2J"
 #define MOVE_CURSOR "\033[0;0H"
 #define SAMPLE_NUM 10000
+#define MAX_ADC 57500
+#define MIN_ADC 7000
+#define MAX_ADC_DIFF 50500
+#define ERROR_MULTIPLIER 100
+#define MAX_SPEED 0
+#define MIN_SPEED 255
 void extend();
 void retract();
 void stop();
+int16_t PID(int16_t[3], int16_t, int16_t[3], uint8_t);
 
 uint8 errorStatus = 0u;
 CY_ISR(RxIsr)
@@ -66,11 +73,9 @@ CY_ISR(SWPin_Control)
     if (InputPin_Read() == 1u)
     {
         if (button) {
-            LED_Write(1);   
-            extend();               
+            LED_Write(1);        
         } else {
             LED_Write(0);   
-            retract();
         }
         
         button = !button;
@@ -92,50 +97,76 @@ int main(void)
     
     PWM3_Start();
 
-    PWM3_WriteCompare1(127);// M5
-    AMux_Select(4);
+    PWM3_WriteCompare1(127);// M5 speed
     
+    AMux_Select(4);
     ADC_DelSig_1_StartConvert();
     
-    static uint16_t buffer[SAMPLE_NUM];
-    uint16_t output = 0;
-    uint32_t counter = 0;
-    bool measured = false;
+    int32_t target = MAX_ADC / 2;
+    int32_t output = 0;
     
-    retract();
+    int16_t PID_output = 0;
+    int16_t integral = 0;
+    int16_t errors[3] = {0};
+    int16_t params[3] = {1, 500, 1};
+    
+    uint8_t counter = 0;
+    uint8_t speed = 0;
     
     printf("%s", CLEAR_STRING);
     printf("%s", MOVE_CURSOR);
-    printf("Wee \r\n");
+    printf("\r");
+    
+    printf("Retracting...");
+    retract();
+    while(button);
+    
+    
+    for(int i = 0; i < 3; i++)
+    {
+        output = ADC_DelSig_1_GetResult32();
+        // rio's code
+        errors[counter] = (int16_t) ((target - output) * ERROR_MULTIPLIER / MAX_ADC_DIFF);
+        counter = (counter + 1) % 3;
+        
+        CyDelay(100);
+    }
     
     for(;;)
     {
-
-        output = ADC_DelSig_1_GetResult16();
-        if (output >= 29491 && output <= 36045 && counter<SAMPLE_NUM)// from ~2.25 to ~2.75
-        {
-            // measure into buffer
-            buffer[counter] = output;
-            counter++;
-        }        
+        output = ADC_DelSig_1_GetResult32();
+        // rio's code
+        errors[counter] = (int16_t) (((target - output) * ERROR_MULTIPLIER) / MAX_ADC_DIFF);
         
-        if (counter == SAMPLE_NUM && measured == false) 
-        {
-            measured = true;
-            printf("Stopped measuring at %d, counter : %d \r\n", output, counter);
+        printf("%s", CLEAR_STRING);
+        printf("%s", MOVE_CURSOR);
+        printf("output %li \r\n", output);
+        printf("Error %d \r\n", errors[counter]);
+        printf("PID %d \r\n", PID_output);
+        printf("speed %d \r\n", speed);
+        
+        integral += errors[counter];
+        PID_output = PID(errors, integral, params, counter);
+        
+        
+        if (abs(PID_output) > ERROR_MULTIPLIER) {
+            speed = MAX_SPEED;
+        } else {
+            speed = (uint8_t) (((int16_t) MIN_SPEED * (ERROR_MULTIPLIER - PID_output)) / ERROR_MULTIPLIER);
         }
         
-        if (output >= 57630 && measured == false)
-        {
-            measured = true;
-            for (int i=0; i<SAMPLE_NUM; i++)
-            {
-                printf("%d, %d\r\n", i, buffer[i]);
-            }
-            printf("Finished measuring at %d, counter : %d \r\n", output, counter);
+        PWM3_WriteCompare1(speed);
+        
+        if (errors[counter] > (ERROR_MULTIPLIER / 200)) {
+            extend();
+        } else if (errors[counter] < -(ERROR_MULTIPLIER / 200)) {
+            retract();
+        } else {
+            stop();
         }
         
-        CyDelayUs(100);
+        counter = (counter + 1) % 3;
+        CyDelay(200);
     }
 }
 
@@ -152,6 +183,13 @@ void retract() {
 void stop() {
     M5_IN1_Write(0U);
     M5_IN2_Write(0U);
+}
+
+int16_t PID(int16_t errors[3], int16_t integral, int16_t params[3], uint8_t counter) {
+    // P = errors[counter] * params[0]
+    // I = integral + errors[counter] * params[1]
+    // D = (errors[counter] - errors[(counter + 1) % 3]) / 2 * params[2]
+    return errors[counter] / params[0] + integral / params[1] + (errors[counter] - errors[(counter + 1) % 3]) / 2 / params[2];
 }
 
 /* [] END OF FILE */
