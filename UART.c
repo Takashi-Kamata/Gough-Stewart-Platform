a6193d447171dd8d37ed67410d99dec93e61bb83
tkam627 <52318237+Takashi-Kamata@users.noreply.github.com>
2022-09-21 22:14:31 +1200

    Init

---
+++ UART.c
@@ -0,0 +1,64 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <UART.h>
+
+uint8	RxBuffer[RxBufferSize];     // Rx circular buffer to hold all incoming command
+uint8  *RxReadIndex	 = RxBuffer;    // pointer to position in RxBuffer to write incoming Rx bytes
+uint8  *RxWriteIndex = RxBuffer;    // pointer to position in RxBuffer to read and process bytes
+
+
+char   *RxStrIndex = RB.RxStr;      // pointer to command string buffer (processed messages)
+                                    // each Rx command consists of: <byte command><string value><CR>
+
+
+CY_ISR(MyRxInt) //interrupt on Rx byte received
+{   
+    //move all available characters from Rx queue to RxBuffer
+    while(UART_ReadRxStatus() & UART_RX_STS_FIFO_NOTEMPTY)
+	{
+		*RxWriteIndex++ = UART_ReadRxData();
+		if (RxWriteIndex >= RxBuffer + RxBufferSize) RxWriteIndex = RxBuffer;      
+	}   
+}
+
+uint8 IsCharReady(void) 
+{
+	return !(RxWriteIndex == RxReadIndex);
+}
+
+uint8 GetRxStr(void)
+{
+    uint8 RxStr_flag = 0;
+    static uint8 Ch;//static?
+   
+	Ch = *RxReadIndex++;       //read next char in buffer
+    if (RxReadIndex >= RxBuffer + RxBufferSize) RxReadIndex = RxBuffer;
+            
+    //if (Ch == EOM_char)
+    if ( (Ch == EOM_CR) || (Ch == EOM_LF) ) //any standard terminator
+    {
+        *RxStrIndex = 0;        //terminate string excluding EOM_char
+        RxStrIndex = RB.RxStr;  //reset pointer
+        if (strlen(RB.RxStr) > 0)//non-empty message received
+        {
+            RxStr_flag  = 1;    //set flag to process message
+        }   
+    }
+    else                        //string body char received
+    {
+        *RxStrIndex++ = Ch;     //build command message   
+        //todo: problem if first char is empty space
+    }   
+
+    return RxStr_flag;        
+}
+/* [] END OF FILE */
---
+++ UART.h
@@ -0,0 +1,36 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+#define RxBufferSize	128     //size of the Rx circular buffer
+#define EOM_CR        0x0D    //message separator char (\r)
+#define EOM_LF        0x0A    //message separator char (\n)
+
+// each Rx command consists of: <char command><string value><CR>
+union TRxBuffer {
+    char   RxStr[20];   // received Rx packet (to hold individual command message)
+    struct {            // anonimous structure
+        char cmd;       // 1-byte command
+        char valstr[19];// command value           
+    };
+} RB;
+
+//=====================================
+//        Function Prototypes 
+//=====================================
+   
+CY_ISR_PROTO(MyRxInt);          // process Rx interrupt
+
+uint8	IsCharReady(void);		// Returns TRUE when a char is ready
+uint8	GetRxStr(void);			// Extract command string from buffer, non-blocking 
+
+/* [] END OF FILE */
